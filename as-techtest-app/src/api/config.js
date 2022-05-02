401843cbb2510ce934724ef067ce1650b65eff80
daniel <daniel.lalasa@gmail.com>
2022-05-03 00:16:26 +0300

    [FEATURE] Implement api call and its config file using Axios

---
+++ as-techtest-app/src/api/config.js
@@ -0,0 +1,5 @@
+const config = {
+    BASE_URL: 'https://www.openstreetmap.org/api/0.6/'
+};
+
+export default config;
--- as-techtest-app/src/api/index.js
+++ as-techtest-app/src/api/index.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+import config from "./config.js";
+const getMapBboxGeoJson = async (position_bounds) => {
+    const {min_lon,min_lat,max_lon,max_lat} = position_bounds;
+	return axios({
+		method: "get",
+		url: config.BASE_URL + "map",
+		params: {
+			bbox: `${min_lon},${min_lat},${max_lon},${max_lat}`,
+		},
+	}).then(res=>res.data);
+};
+
+export { getMapBboxGeoJson };
